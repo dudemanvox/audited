008ec53f901d195fcac2a930d06337ba79b2287b
Tommy Bregar <tommybregar@gmail.com>
2016-09-20 10:56:40 -0600

    Add accessor for for the current_admin_method and default value.

--- lib/audited.rb
+++ lib/audited.rb
@@ -2,7 +2,7 @@ module Audited
   VERSION = '3.0.0'
 
   class << self
-    attr_accessor :ignored_attributes, :current_user_method, :audit_class
+    attr_accessor :ignored_attributes, :current_user_method, :current_admin_method, :audit_class
 
     def store
       Thread.current[:audited_store] ||= {}
@@ -12,4 +12,5 @@ module Audited
   @ignored_attributes = %w(lock_version created_at updated_at created_on updated_on)
 
   @current_user_method = :current_user
+  @current_admin_method = :curren_admin
 end
